4fe134395dce8b74323d5e60d625a7795ee98e01
ezafati <padawan@padawan>
2020-01-04 20:23:40 +0100

    add json file as material database

---
+++ src/GlobalObjects/__init__.py
@@ -0,0 +1,1 @@
+import GlobalObjects.MainObjects
---
+++ tests/add_mate_test.py
@@ -0,0 +1,19 @@
+import os.path
+from GlobalObjects.MainObjects import Material
+
+path1, *_ = os.path.split(os.path.abspath('./'))
+dbpath = os.path.join(path1, 'src/matdb/matdb.json')
+
+
+'''add linear elsatic material'''
+mate = Material('Linear_Elastic', {'Young': 1E9, 'Poisson': 0.25})
+mate.add_material(dbpath)
+
+'''add perfect elatsoplastic material'''
+
+mate = Material('ElastoPlastic_Perfect', {'Young': 1E9, 'Poisson': 0.25}, {'siglimit': 10e6})
+mate.add_material(dbpath)
+
+mate = Material('Linear_Elastic')
+mate.get_material(dbpath)
+print(mate)
